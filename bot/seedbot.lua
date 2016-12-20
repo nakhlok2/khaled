1216b7bb17ee0977e5b8381cc6bdcc1bdfc1def1
nakhlok2 <darkhal98@gmail.com>
2016-12-20 06:46:43 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {83284623},--Sudo users
+    sudo_users = {83284623,312054704},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
